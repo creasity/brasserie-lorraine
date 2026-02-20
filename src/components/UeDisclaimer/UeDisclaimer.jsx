73f8f6c14c008658016a75330b652f4433456926
creasity <59027260+creasity@users.noreply.github.com>
2026-02-20 20:37:37 +0700

    Update UeDisclaimer.jsx

--- src/components/UeDisclaimer/UeDisclaimer.jsx
+++ src/components/UeDisclaimer/UeDisclaimer.jsx
@@ -9,11 +9,11 @@ export default function UeDisclaimer() {
           Face aux surcoûts liés à l’éloignement géographique et au transport maritime, l’aide au fret UE constitue un appui déterminant. Elle contribue à atténuer les charges liées à l’importation des matières premières indispensables à notre production et facilite l’exportation de nos créations vers le marché européen.
           C’est un véritable coup de pouce pour notre activité : nous pouvons continuer à produire avec passion, renforcer notre compétitivité et valoriser la production martiniquaise sur le marché national.
       </p>
-			<div style={{ display: 'flex', gap: '16px', marginTop: '24px', flexWrap: 'wrap' }}>
-			  <img src="/assets/La-fabrication-de-la-biere-_-ingredient-malt-dorge (1).jpg" alt="..." style={{ flex: '1 1 200px', objectFit: 'cover'}} />
-			  <img src="/assets/Photo-chaine-de-prod.jpg" alt="..." style={{ flex: '1 1 200px', objectFit: 'cover' }} />
-			  <img src="/assets/bottles_enhanced_8k.jpg" alt="..." style={{ flex: '1 1 200px', objectFit: 'cover' }} />
-			</div>
+		<div style={{ display: 'flex', gap: '16px', marginTop: '24px', flexWrap: 'wrap', justifyContent: 'center', maxWidth: '600px', margin: '24px auto 0' }}>
+		  <img src="/assets/La-fabrication-de-la-biere-_-ingredient-malt-dorge (1).jpg" alt="..." style={{ flex: '1 1 150px', objectFit: 'cover', maxWidth: '100%' }} />
+		  <img src="/assets/Photo-chaine-de-prod.jpg" alt="..." style={{ flex: '1 1 150px', objectFit: 'cover', maxWidth: '100%' }} />
+		  <img src="/assets/bottles_enhanced_8k.jpg" alt="..." style={{ flex: '1 1 150px', objectFit: 'cover', maxWidth: '100%' }} />
+		</div>
       <p>
           Pour la période de janvier 2023 à décembre 2024, ce dispositif représente un montant de 972 258,46€.
            Ce soutien est rendu possible grâce au cofinancement de l’Union européenne et de la Collectivité Territoriale de Martinique, dans le cadre du Programme Opérationnel Martinique 2021-2027, illustrant l’engagement des institutions en faveur du développement économique local.
